dd7451e93988b7070813fb20afe2d84ca4893340
Kirtika Ruchandani <kirtibr@gmail.com>
2009-11-15 20:05:27 +0530

    Jsut pushing the mess that is presently there

--- include/lexer.h
+++ include/lexer.h
@@ -5,16 +5,15 @@
 #ifndef LEXER_H
 #define LEXER_H
 
-#include "circuit.h"
 
 /*
- * Define globla functions to be used by lexer 
+ * Define global functions to be used by lexer 
  * to add elements to the circuit
  */
-extern bool Lexer_AddWire(char *inName,int type);
-extern bool Lexer_AddGate(GateType type, char *name,char* output,char **inputs,int numSignals);
+extern int Lexer_AddWire(char *inName,int type);
+extern int Lexer_AddGate(GateType type, char *name,char* output,char **inputs,int numSignals);
 
-bool lexer(void);	// should be renamed to the yacc lexer function
+int lexer(void);	// should be renamed to the yacc lexer function
 
 #endif /* ifndef LEXER_H */
 
