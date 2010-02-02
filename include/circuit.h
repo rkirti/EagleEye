b6d976a948ccdb206142358f27493d5661d39585
cs07b040 <rkirti@hubble.cse.iitm.ac.in>
2010-02-02 09:59:37 +0530

    Moved the enums to a separate place

--- include/circuit.h
+++ include/circuit.h
@@ -17,26 +17,14 @@
 #include <sstream>
 #include <fstream>
 #include <vector>
-
+#include  "enumdef.h"
 
 
 using namespace std;
 
-enum Value{ZERO=0b0000,ONE=0b1111,U=0b0101,D=0b1100,DBAR=0b0011,ZEROBYU=0b0001,UBYZERO=0b0100,ONEBYU=0b1101,UBYONE=0b0111,UBAR=0b1010};
-
-const Value ControlValues[7] = {ZERO,ONE,U,ZERO,ONE,U,U};
-
-const Value InversionValues[7] = {ZERO,ZERO,U,ONE,ONE,U,U};
-
-enum CktElement{WIRE=0,GATE,UNKNOWN};
-
-enum GateType{AND=0,OR,NOT,NAND,NOR,XOR,BUF};
-
-//const char* Evals[] = {AND,OR,};
-
-enum WireType{PI=0,PO=1,CONNECTION=2};
-
 class Wire;
+
+
 
 /*Generic Evaluate function table*/
 typedef Value (*GateEvaluate)(list<Wire*> inputs);
